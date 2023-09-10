671de1a75388a092b72b6bc5f4d777a88f7d3ad4
Ana Carla Quallio Rosa <92097155+anacarlaquallio@users.noreply.github.com>
2023-09-10 18:46:18 -0300

    avanços

---
+++ src/app/empresa/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import Sidebar from "@/components/ui/Sidebar"
+import { Separator } from "@/components/ui/separator";
+import { Card, CardHeader, CardBody, Spacer } from "@nextui-org/react";
+
+
+export default function Projetos() {
+    return (
+        <div>
+            <div className="layout">
+                <Sidebar></Sidebar>
+                <div>
+                    <h1>Início</h1>
+                    <Separator />
+                    <div>
+                        <Spacer x={4} />
+                        <Card className="py-4 bg-card">
+                            <CardHeader className="pb-0 pt-2 px-4 flex-col items-center">
+                                <h4 className="font-bold text-large color-card">Empresa 1</h4>
+                                <Spacer x={3} />
+                                <small className="text-default-500">Localização</small>
+                            </CardHeader>
+                            <CardBody className="overflow-visible py-2">
+                            </CardBody>
+                        </Card>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
