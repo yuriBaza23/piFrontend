55bd7f49e51594f5647840da2c2d2899611837f9
Ana Carla Quallio Rosa <92097155+anacarlaquallio@users.noreply.github.com>
2023-09-17 22:05:25 -0300

    termino componentes

--- src/app/incubadora/busca/page.tsx
+++ src/app/incubadora/busca/page.tsx
@@ -5,6 +5,15 @@ import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
 import { Spacer } from "@nextui-org/react";
 import { AiOutlineSearch } from "react-icons/ai";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
 
 export default function Busca_Empresa() {
 
@@ -22,7 +31,54 @@ export default function Busca_Empresa() {
                             <Button >
                                 <AiOutlineSearch className="h-4 w-4" />
                             </Button>
+                            <Button >
+                                All
+                            </Button>
                         </div>
+                        <Spacer x={4} />
+                        <Card className="w-[350px] bg-card2">
+                            <CardHeader>
+                                <CardDescription className="color-card">Confira os dados da empresa:</CardDescription>
+                            </CardHeader>
+                            <CardContent>
+                                <form>
+                                    <div className="grid w-full items-center gap-4">
+                                    <div className="flex flex-col space-y-1.5">
+                                            <Label htmlFor="id">ID da empresa:</Label>
+                                            <div className="rounded-md border px-4 py-2 font-mono text-sm shadow-sm">
+                                                ID da empresa
+                                            </div>
+                                        </div>
+                                        <div className="flex flex-col space-y-1.5">
+                                            <Label htmlFor="name">Nome da empresa:</Label>
+                                            <div className="rounded-md border px-4 py-2 font-mono text-sm shadow-sm">
+                                                Nome da empresa
+                                            </div>
+                                        </div>
+                                        <div className="flex flex-col space-y-1.5">
+                                            <Label htmlFor="email">Email da empresa:</Label>
+                                            <div className="rounded-md border px-4 py-2 font-mono text-sm shadow-sm">
+                                                Email da empresa
+                                            </div>
+                                        </div>
+                                        <div className="flex flex-col space-y-1.5">
+                                            <Label htmlFor="cnpj">CNPJ da empresa:</Label>
+                                            <div className="rounded-md border px-4 py-2 font-mono text-sm shadow-sm">
+                                                CNPJ
+                                            </div>
+                                        </div>
+                                        <div className="flex flex-col space-y-1.5">
+                                            <Label htmlFor="cnpj">Nome do proprietário</Label>
+                                            <div className="rounded-md border px-4 py-2 font-mono text-sm shadow-sm">
+                                                Proprietário
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+                            </CardContent>
+
+                        </Card>
+
                     </div>
                 </div>
             </div>
