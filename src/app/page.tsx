0ab8ed946dcb1ae670bca7dd6143881aec6760b3
Ana Carla Quallio Rosa <92097155+anacarlaquallio@users.noreply.github.com>
2023-12-04 17:04:55 -0300

    Login inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 "use client";
 
-import MainTable from '@/components/tabela/MainTable';
 import Login from './login/page';
 
 export default function Home() {
     return (
-        <div className="layout-tabela">
-        <MainTable/>
-        </div>
+        <Login />
     );
 }
