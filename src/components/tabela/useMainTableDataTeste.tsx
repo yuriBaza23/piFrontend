f69871eb237950ae8a8e9fe08d02a8020f0ec0f3
Ana Carla Quallio Rosa <92097155+anacarlaquallio@users.noreply.github.com>
2023-12-04 10:27:05 -0300

    receita bruta

--- src/components/tabela/useMainTableDataTeste.tsx
+++ src/components/tabela/useMainTableDataTeste.tsx
@@ -32,27 +32,27 @@ const Testao = create<SellerYearProps>()(
     months: yearData
       ? (Object.keys(Object.values(yearData)[0]) as months[])
       : [],
-      colTypes: yearData
+    colTypes: yearData
       ? Object.assign(
-          Object.values(Object.values(Object.values(yearData)[0]))[0]
-        )
+        Object.values(Object.values(Object.values(yearData)[0]))[0]
+      )
       : [],
-      dataMatrix: Object.assign(
-        Object.values(
-          Object.values(Object.values(Object.values(yearData[2023])))
-        ).map((col) =>
-          Object.values(col)
-            .map((c) =>
-              c.map((d) => {
-                return {
-                  value: d.value,
-                  isEditable: d.isEditable,
-                };
-              })
-            )
-            .flat()
-        )
-      ),
+    dataMatrix: Object.assign(
+      Object.values(
+        Object.values(Object.values(Object.values(yearData[2023])))
+      ).map((col) =>
+        Object.values(col)
+          .map((c) =>
+            c.map((d) => {
+              return {
+                value: d.value,
+                isEditable: d.isEditable,
+              };
+            })
+          )
+          .flat()
+      )
+    ),
 
     changeCellValue: (
       year: string,
@@ -66,6 +66,39 @@ const Testao = create<SellerYearProps>()(
         newData[year][month][col] = newData[year][month][col].map((c: ColData) =>
           c.subType === colType ? { ...c, value: value } : c
         ) as ColData[];
+
+        // Receita
+        const subtipoRecorrente = "Recorrente";
+        const subtipoNaoRecorrente = "Não recorrente";
+        const subtipoBruta = "Bruta";
+        
+        if (col === "receita" && (colType === subtipoRecorrente || colType === subtipoNaoRecorrente)) {
+          // Obtendo os valores dos subtipos "Recorrente" e "Não recorrente"
+          const valorRecorrente = newData[year][month][col].find(
+            (c: ColData) => c.subType === subtipoRecorrente
+          )?.value ?? 0;
+        
+          const valorNaoRecorrente = newData[year][month][col].find(
+            (c: ColData) => c.subType === subtipoNaoRecorrente
+          )?.value ?? 0;
+        
+          // Calculando a soma dos valores
+          const resultado = valorRecorrente + valorNaoRecorrente;
+        
+          // Atualizando o valor do subtipo "Bruta"
+          const itemBruta = newData[year][month][col].find(
+            (c: ColData) => c.subType === subtipoBruta
+          );
+        
+          if (itemBruta) {
+            itemBruta.value = resultado;
+          }
+          // Retorne o estado atualizado
+          return {
+            ...state,
+            mainData: newData,
+          };
+        }
         return {
           ...state,
           mainData: newData,
@@ -76,3 +109,4 @@ const Testao = create<SellerYearProps>()(
 );
 
 export default Testao;
+
